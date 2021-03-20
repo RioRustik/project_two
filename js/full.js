bc2593a223184fdfddc376944e967fa38679cd28
SMORODINA\Rusta <possession1931@gmail.com>
2021-03-20 21:50:44 +0400

    no message

---
+++ js/full.js
@@ -0,0 +1,44 @@
+// tV- temporary variable
+    // function f() {
+    //     fd1.value = fd1.value.replace(/\s/g, '');
+    //     fd2.value = fd2.value.replace(/\s/g, '');
+    //     fd3.value = fd3.value.replace(/\s/g, '');
+    //     fd4.value = fd4.value.replace(/\s/g, '');
+    //     fd5.value = fd5.value.replace(/\s/g, '');
+    // } 
+//   document.querySelector(".inp").oninput = function () {
+//      this.value = this.value.trim();
+//   };
+
+// let srct = "rrwrttev";
+// let arr = [];
+// let arr2 = [];
+// let arr3 = [];
+// let chart;
+// let count = 0;
+
+// for(let i = 0; i<srct.length; i++)
+// {
+//     arr[i] = srct.charAt(i);
+// }
+
+// console.log(arr);
+
+
+
+// for(let i = 0; i<arr.length; i++)
+// { 
+//     chart=arr[i];
+//     for(let j = 0; j<arr.length; j++)
+//     {
+//         if(chart==arr[j])
+//         {
+//             count++;
+//         }
+        
+//     }
+//     arr2[i]=count;
+//     count=0;
+// }
+// console.log(count);
+// console.log(arr2);
