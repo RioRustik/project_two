9b2393cbcec65a12d05b3a979031bce76efb3c30
SMORODINA\Rusta <possession1931@gmail.com>
2021-03-27 12:58:34 +0400

    no message

--- js/taskFifth.js
+++ js/taskFifth.js
@@ -2,6 +2,7 @@ let films = [];
 
 class Film
 {
+    // добавить атрибуты ввода фильма
     constructor(title, country) 
     {
         this.title = title;
@@ -16,12 +17,14 @@ document.getElementById('buttonServisForm').onclick = function()
         if(sessionStorage.getItem('str')){
             films = JSON.parse(sessionStorage.getItem('str'));
             films.push(new Film(
+                // добавить атрибуты ввода фильма
                 title = document.getElementById('inputFirstServisForm').value,
                 country = document.getElementById('inputSecondServisForm').value
             ));
             sessionStorage.setItem('str', JSON.stringify(films));
         }else{
             films.push(new Film(
+                // добавить атрибуты ввода фильма
                 title = document.getElementById('inputFirstServisForm').value,
                 country = document.getElementById('inputSecondServisForm').value
             ));
@@ -37,19 +40,41 @@ if(sessionStorage.getItem('str'))
     films = JSON.parse(sessionStorage.getItem('str'));
     for(let i = 0; i < films.length; i++)
     {
+        // проименовать переменные адекватно
         let d=document.createElement('div');
-        d.classList.add('s2')
+        d.classList.add('picture');
             let b=document.createElement('div');
-            b.classList.add('s3');
-            b.innerHTML = films[i].title;
-
-            let w=document.createElement('div');
-            w.classList.add('s3');
-            w.innerHTML = films[i].country;
+            b.classList.add('front');
+            b.classList.add('favorite-f');
+                let w=document.createElement('h2');
+                w.classList.add('s23');
+                w.innerHTML = films[i].title;
         document.getElementById('vid').appendChild(d);
-        document.getElementsByClassName('s2')[i].appendChild(b);
-        document.getElementsByClassName('s2')[i].appendChild(w);
+        document.getElementsByClassName('picture')[i].appendChild(b);
+        document.getElementsByClassName('front')[i].appendChild(w);
         
-        console.log(films[i])
+        
+        let r=document.createElement('div');
+        r.classList.add('back');
+        r.classList.add('favorite-f-back');
+            let t=document.createElement('div');
+            t.classList.add('s33');
+                let y=document.createElement('div');
+                y.classList.add('s4');
+                    let u=document.createElement('div');
+                    u.classList.add('s5');
+                    u.innerHTML = "Страна";
+                    let o=document.createElement('div');
+                    o.classList.add('s6');
+                    o.innerHTML = films[i].country;
+
+        document.getElementsByClassName('picture')[i].appendChild(r);
+        document.getElementsByClassName('back')[i].appendChild(t);
+        document.getElementsByClassName('s33')[i].appendChild(y);
+        document.getElementsByClassName('s4')[i].appendChild(u);
+        document.getElementsByClassName('s4')[i].appendChild(o);
+        
+        // дописать ввод вех остальных элементов аналогично вводу country
+        // продумать как добавлять картинку не в innerHTML а в свойство определенного тиля как его атрибут
     }
 }
