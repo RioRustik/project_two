af7ea2b4ad3bdc964694f5afec51ebc249074101
SMORODINA\Rusta <possession1931@gmail.com>
2021-04-01 19:35:35 +0400

    no message

--- js/full.js
+++ js/full.js
@@ -84,3 +84,161 @@
     //     // console.log(d) 
 
     // }
+
+    // ________________________________________________________________________________________
+
+      // document.getElementById('vid').appendChild(mainFront);
+    // document.getElementsByClassName('picture')[i].appendChild(labelTitle);
+    // document.getElementsByClassName('front')[i].appendChild(title1);
+  
+
+    // // проименовать переменные адекватно
+    // let d=document.createElement('div');
+    // d.classList.add('picture');
+    //     let b=document.createElement('div');
+    //     b.classList.add('front');
+    //     b.classList.add('favorite-f');
+    //         let w=document.createElement('h2');
+    //         w.classList.add('s23');
+    //         w.innerHTML = films[i].title;
+
+       
+    
+    
+            // document.getElementsByClassName('picture')[i].appendChild(mainBack);
+            // document.getElementsByClassName('back')[i].appendChild(properties);
+            // document.getElementsByClassName('s33')[i].appendChild(propCountry);
+            // document.getElementsByClassName('s33')[i].appendChild(propGenre);
+            // document.getElementsByClassName('s33')[i].appendChild(propDirector);
+            // document.getElementsByClassName('s33')[i].appendChild(propScenario);
+            // document.getElementsByClassName('s33')[i].appendChild(propProducer);
+            // document.getElementsByClassName('s33')[i].appendChild(propOperator);
+            // document.getElementsByClassName('s33')[i].appendChild(propComposer);
+            // document.getElementsByClassName('s33')[i].appendChild(propBudget);
+            // document.getElementsByClassName('s33')[i].appendChild(propMoney);
+            // document.getElementsByClassName('s33')[i].appendChild(propRating);
+            // document.getElementsByClassName('s33')[i].appendChild(propLength);
+            // document.getElementsByClassName('s33')[i].appendChild(propDate);
+    
+    
+            // document.getElementsByClassName('s4')[i].appendChild(labelCountry);
+            // document.getElementsByClassName('s4')[i].appendChild(country1);
+            // document.getElementsByClassName('s4')[i].appendChild(labelGenre);
+            // document.getElementsByClassName('s4')[i].appendChild(genre1);
+            // document.getElementsByClassName('s4')[i].appendChild(labelDirector);
+            // document.getElementsByClassName('s4')[i].appendChild(director1);
+            // document.getElementsByClassName('s4')[i].appendChild(labelScenario);
+            // document.getElementsByClassName('s4')[i].appendChild(scenario1);
+            // document.getElementsByClassName('s4')[i].appendChild(labelProducer);
+            // document.getElementsByClassName('s4')[i].appendChild(producer1);
+            // document.getElementsByClassName('s4')[i].appendChild(labelOperator);
+            // document.getElementsByClassName('s4')[i].appendChild(operator1);
+            // document.getElementsByClassName('s4')[i].appendChild(labelComposer);
+            // document.getElementsByClassName('s4')[i].appendChild(composer1);
+            // document.getElementsByClassName('s4')[i].appendChild(labelBudget);
+            // document.getElementsByClassName('s4')[i].appendChild(budget1);
+            // document.getElementsByClassName('s4')[i].appendChild(labelMoney);
+            // document.getElementsByClassName('s4')[i].appendChild(money1);
+            // document.getElementsByClassName('s4')[i].appendChild(labelRating);
+            // document.getElementsByClassName('s4')[i].appendChild(rating1);
+            // document.getElementsByClassName('s4')[i].appendChild(labelLength);
+            // document.getElementsByClassName('s4')[i].appendChild(length1);
+            // document.getElementsByClassName('s4')[i].appendChild(labelDate);
+            // document.getElementsByClassName('s4')[i].appendChild(date1);
+            
+            // дописать ввод вех остальных элементов аналогично вводу country
+            // продумать как добавлять картинку не в innerHTML а в свойство определенного тиля как его атрибут
+    //     }
+    // }
+
+
+
+    // -------------------------------------------------------------------------------------        
+    // let r=document.createElement('div');
+    // r.classList.add('back');
+    // r.classList.add('favorite-f-back');
+    //     let t=document.createElement('div');
+    //     t.classList.add('s33');
+
+    //         let y=document.createElement('div');
+    //         y.classList.add('s4');
+
+    //             let u=document.createElement('div');
+    //             u.classList.add('s5');
+    //             u.innerHTML = "Страна";
+    //             let o=document.createElement('div');
+    //             o.classList.add('s6');
+    //             o.innerHTML = films[i].country;
+    //             // o.className
+                
+    //             // let v=document.createElement('button');
+    //             // v.classList.add('s6');
+    //             // v.innerHTML = "del";
+               
+
+    //             // v.onclick = function () { 
+    //             //         b.parentNode.remove();
+    //             //         films.splice(i, 1);
+    //             //         sessionStorage.setItem('str', JSON.stringify(films)); 
+    //             // };
+
+
+    // // document.getElementsByClassName('s4')[i].appendChild(v);
+
+    // // let p=document.createElement('div');
+    // //     p.classList.add('s33');
+    //         let l=document.createElement('div');
+    //         l.classList.add('s4');
+    //             let m=document.createElement('div');
+    //             m.classList.add('s5');
+    //             m.innerHTML = "Жанр";
+    //             let c=document.createElement('div');
+    //             c.classList.add('s6');
+    //             c.innerHTML = films[i].genre;
+    //             // o.className
+                
+    //             let n=document.createElement('button');
+    //             n.classList.add('s6');
+    //             n.innerHTML = "del";
+               
+
+    //             // n.onclick = function () { 
+    //             //         b.parentNode.remove();
+    //             //         films.splice(i, 1);
+    //             //         sessionStorage.setItem('str', JSON.stringify(films)); 
+    //             // };
+
+    // document.getElementById('vid').append(d);
+    // d.append(b);
+    // b.append(w);
+
+    // d.append(r);
+    // r.append(t);
+    // t.append(y);
+    // y.append(u);
+    // y.append(o);
+
+    // t.append(l);
+    // l.append(m);
+    // l.append(c);
+    // document.getElementById('vid').append(d)
+    // document.getElementsByClassName('picture')[i].append(b);
+    // document.getElementsByClassName('front')[i].append(w);
+
+    // document.getElementsByClassName('picture')[i].append(r);
+    // document.getElementsByClassName('back')[i].append(t);
+    // document.getElementsByClassName('s33')[i].append(y);
+    // document.getElementsByClassName('s33')[i].append(l);
+    //  // document.getElementsByClassName('s33')[i].appendChild(l);
+    // document.getElementsByClassName('s4')[i].append(u); /*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+    // // document.getElementsByClassName('s4')[i].append(o); 
+
+    // document.getElementsByClassName('picture')[i].appendChild(r);
+    // document.getElementsByClassName('back')[i].appendChild(t);
+    // document.getElementsByClassName('s4')[i].appendChild(l);
+    
+    // document.getElementsByClassName('s4')[i].append(m);
+    // document.getElementsByClassName('s4')[i].append(c);
+    // document.getElementsByClassName('s4')[i].appendChild(n);
+
+
--- js/taskFifth.js
+++ js/taskFifth.js
@@ -1,12 +1,46 @@
 let films = [];
 
+// document.getElementById('key').onclick = function()
+// {
+//     if(document.getElementById('key').checked){
+//         func2();
+//     }
+//     else
+//     {
+//         func1();
+//     }
+// }
+    
+// function func1(){
+//     document.getElementsByClassName('popup')[0].classList.add('ss2');
+// }
+// //     document.querySelector('.ss1').classList.add('ss2');
+// // }
+// function func2(){
+//     document.getElementsByClassName('popup')[0].classList.add('ss1');
+
+//     // document.querySelector('ss2').classList.add('ss2');
+// }
+
+
 class Film
 {
-    // добавить атрибуты ввода фильма
-    constructor(title, country) 
+    constructor(title, country, genre, director, scenario, producer, operator, composer,  budget, money, rating, length, date, poster)
     {
         this.title = title;
         this.country = country;
+        this.genre = genre;
+        this.director = director;
+        this.scenario = scenario;
+        this.producer = producer;
+        this.operator = operator;
+        this.composer = composer;
+        this.budget = budget;
+        this.money = money;
+        this.rating = rating;
+        this.length = length;
+        this.date = date;
+        this.poster = poster;
     }
 }
 
@@ -17,26 +51,64 @@ document.getElementById('buttonServisForm').onclick = function()
         if(sessionStorage.getItem('str')){
             films = JSON.parse(sessionStorage.getItem('str'));
             films.push(new Film(
-                // добавить атрибуты ввода фильма
                 title = document.getElementById('inputFirstServisForm').value,
-                country = document.getElementById('inputSecondServisForm').value
+                country = document.getElementById('inputSecondServisForm').value,
+                genre = document.getElementById('inputThirdServisForm').value,
+                director = document.getElementById('inputFourthServisForm').value,
+                scenario = document.getElementById('inputFifthServisForm').value,
+                producer = document.getElementById('inputSixthServisForm').value,
+                operator = document.getElementById('inputSeventhServisForm').value,
+                composer = document.getElementById('inputEighthServisForm').value,
+                budget = document.getElementById('inputNinthServisForm').value,
+                money = document.getElementById('inputTenthServisForm').value,
+                rating = document.getElementById('inputEleventhServisForm').value,
+                length = document.getElementById('inputTwelfthServisForm').value,
+                date = document.getElementById('inputThirteenthServisForm').value,
+                poster = document.getElementById('inputFourteenthServisForm').value
+
             ));
             sessionStorage.setItem('str', JSON.stringify(films));
-            test();
-        }else{
+            i = films.length-1;
+            myfunc(i);
+        }else
+        {
             films.push(new Film(
-                // добавить атрибуты ввода фильма
                 title = document.getElementById('inputFirstServisForm').value,
-                country = document.getElementById('inputSecondServisForm').value
+                country = document.getElementById('inputSecondServisForm').value,
+                genre = document.getElementById('inputThirdServisForm').value,
+                director = document.getElementById('inputFourthServisForm').value,
+                scenario = document.getElementById('inputFifthServisForm').value,
+                producer = document.getElementById('inputSixthServisForm').value,
+                operator = document.getElementById('inputSeventhServisForm').value,
+                composer = document.getElementById('inputEighthServisForm').value,
+                budget = document.getElementById('inputNinthServisForm').value,
+                money = document.getElementById('inputTenthServisForm').value,
+                rating = document.getElementById('inputEleventhServisForm').value,
+                length = document.getElementById('inputTwelfthServisForm').value,
+                date = document.getElementById('inputThirteenthServisForm').value,
+                poster = document.getElementById('inputFourteenthServisForm').value
+
             ));
             sessionStorage.setItem('str', JSON.stringify(films)); 
-            test();
-
         }
         document.getElementById("inputFirstServisForm").value = "";
-        document.getElementById("inputSecondServisForm").value = "";     
+        document.getElementById("inputSecondServisForm").value = "";    
+        document.getElementById('inputThirdServisForm').value = "";
+        document.getElementById('inputFourthServisForm').value = "";
+        document.getElementById('inputFifthServisForm').value = "";
+        document.getElementById('inputSixthServisForm').value = "";
+        document.getElementById('inputSeventhServisForm').value = "";
+        document.getElementById('inputEighthServisForm').value = "";
+        document.getElementById('inputNinthServisForm').value = "";
+        document.getElementById('inputTenthServisForm').value = "";
+        document.getElementById('inputEleventhServisForm').value = "";
+        document.getElementById('inputTwelfthServisForm').value = "";
+        document.getElementById('inputThirteenthServisForm').value = "";
+        document.getElementById('inputFourteenthServisForm').value = "";    
     }
 }
+test();
+
 function test()
 {
     if(sessionStorage.getItem('str'))
@@ -44,45 +116,196 @@ function test()
         films = JSON.parse(sessionStorage.getItem('str'));
         for(let i = 0; i < films.length; i++)
         {
-            // проименовать переменные адекватно
-            let d=document.createElement('div');
-            d.classList.add('picture');
-                let b=document.createElement('div');
-                b.classList.add('front');
-                b.classList.add('favorite-f');
-                    let w=document.createElement('h2');
-                    w.classList.add('s23');
-                    w.innerHTML = films[i].title;
-    
-            document.getElementById('vid').appendChild(d);
-            document.getElementsByClassName('picture')[i].appendChild(b);
-            document.getElementsByClassName('front')[i].appendChild(w);
-            
-            
-            let r=document.createElement('div');
-            r.classList.add('back');
-            r.classList.add('favorite-f-back');
-                let t=document.createElement('div');
-                t.classList.add('s33');
-                    let y=document.createElement('div');
-                    y.classList.add('s4');
-                        let u=document.createElement('div');
-                        u.classList.add('s5');
-                        u.innerHTML = "Страна";
-                        let o=document.createElement('div');
-                        o.classList.add('s6');
-                        o.innerHTML = films[i].country;
-    
-            document.getElementsByClassName('picture')[i].appendChild(r);
-            document.getElementsByClassName('back')[i].appendChild(t);
-            document.getElementsByClassName('s33')[i].appendChild(y);
-            document.getElementsByClassName('s4')[i].appendChild(u);
-            document.getElementsByClassName('s4')[i].appendChild(o);
-            
-            // дописать ввод вех остальных элементов аналогично вводу country
-            // продумать как добавлять картинку не в innerHTML а в свойство определенного тиля как его атрибут
-            // 
+            myfunc(i);         
         }
     }
 }
-test();
+function myfunc(i)
+{    
+    // блок front
+    let mainFront=document.createElement('div');
+    mainFront.classList.add('picture');
+        let labelTitle=document.createElement('div');
+        labelTitle.classList.add('front');
+        labelTitle.classList.add('favorite-f');
+        if(films[i].poster=="")
+            labelTitle.style.background = "linear-gradient( rgba(0, 0, 0, 0), rgba(128, 125, 125, 0.7) ), #232525";
+        else
+            labelTitle.style.backgroundImage=`url(${films[i].poster})`;   
+        let title1=document.createElement('h2');
+        title1.classList.add('s23');
+        title1.innerHTML = films[i].title;
+    // блок back
+    let mainBack=document.createElement('div');
+    mainBack.classList.add('back');
+    mainBack.classList.add('favorite-f-back');
+        let properties=document.createElement('div');
+        properties.classList.add('s33');
+
+    // блок страна
+    let propCountry=document.createElement('div');
+    propCountry.classList.add('s4');
+        let labelCountry=document.createElement('div');
+        labelCountry.classList.add('s5');
+        labelCountry.innerHTML = "Страна:";
+        let country1=document.createElement('div');
+        country1.classList.add('s6');
+        country1.innerHTML = films[i].country;
+    // блок жанр
+    let propGenre=document.createElement('div');
+    propGenre.classList.add('s4');
+        let labelGenre=document.createElement('div');
+        labelGenre.classList.add('s5');
+        labelGenre.innerHTML = "Жанр:";
+        let genre1=document.createElement('div');
+        genre1.classList.add('s6');
+        genre1.innerHTML = films[i].genre;
+    // блок режиссер
+    let propDirector=document.createElement('div');
+    propDirector.classList.add('s4');
+        let labelDirector=document.createElement('div');
+        labelDirector.classList.add('s5');
+        labelDirector.innerHTML = "Режиссёр:";
+        let director1=document.createElement('div');
+        director1.classList.add('s6');
+        director1.innerHTML = films[i].director;
+    // блок сценарий
+    let propScenario=document.createElement('div');
+    propScenario.classList.add('s4');
+        let labelScenario=document.createElement('div');
+        labelScenario.classList.add('s5');
+        labelScenario.innerHTML = "Сценарий:";
+        let scenario1=document.createElement('div');
+        scenario1.classList.add('s6');
+        scenario1.innerHTML = films[i].scenario;
+    // блок продюсер
+    let propProducer=document.createElement('div');
+    propProducer.classList.add('s4');
+        let labelProducer=document.createElement('div');
+        labelProducer.classList.add('s5');
+        labelProducer.innerHTML = "Продюсер:";
+        let producer1=document.createElement('div');
+        producer1.classList.add('s6');
+        producer1.innerHTML = films[i].producer;
+    // блок оператор
+    let propOperator=document.createElement('div');
+    propOperator.classList.add('s4');
+        let labelOperator=document.createElement('div');
+        labelOperator.classList.add('s5');
+        labelOperator.innerHTML = "Оператор:";
+        let operator1=document.createElement('div');
+        operator1.classList.add('s6');
+        operator1.innerHTML = films[i].operator;          
+    // блок композитор
+    let propComposer=document.createElement('div');
+    propComposer.classList.add('s4');
+        let labelComposer=document.createElement('div');
+        labelComposer.classList.add('s5');
+        labelComposer.innerHTML = "Композитор:";
+        let composer1=document.createElement('div');
+        composer1.classList.add('s6');
+        composer1.innerHTML = films[i].composer;         
+    // блок бюджет
+    let propBudget=document.createElement('div');
+    propBudget.classList.add('s4');
+        let labelBudget=document.createElement('div');
+        labelBudget.classList.add('s5');
+        labelBudget.innerHTML = "Бюджет:";
+        let budget1=document.createElement('div');
+        budget1.classList.add('s6');
+        budget1.innerHTML = films[i].budget;
+    // блок сборы
+    let propMoney=document.createElement('div');
+    propMoney.classList.add('s4');
+        let labelMoney=document.createElement('div');
+        labelMoney.classList.add('s5');
+        labelMoney.innerHTML = "Сборы:";
+        let money1=document.createElement('div');
+        money1.classList.add('s6');
+        money1.innerHTML = films[i].money;
+    // блок рейтинг
+    let propRating=document.createElement('div');
+    propRating.classList.add('s4');
+        let labelRating=document.createElement('div');
+        labelRating.classList.add('s5');
+        labelRating.innerHTML = "Рейтинг:";
+        let rating1=document.createElement('div');
+        rating1.classList.add('s6');
+        rating1.innerHTML = films[i].rating;
+    // блок длительность
+    let propLength=document.createElement('div');
+    propLength.classList.add('s4');
+        let labelLength=document.createElement('div');
+        labelLength.classList.add('s5');
+        labelLength.innerHTML = "Длительность:";
+        let length1=document.createElement('div');
+        length1.classList.add('s6');
+        length1.innerHTML = films[i].length;    
+    // блок дата
+    let propDate=document.createElement('div');
+    propDate.classList.add('s4');
+        let labelDate=document.createElement('div');
+        labelDate.classList.add('s5');
+        labelDate.innerHTML = "Дата выхода:";
+        let date1=document.createElement('div');
+        date1.classList.add('s6');
+        date1.innerHTML = films[i].date;
+
+    // блок front
+    document.getElementById('vid').append(mainFront);
+    mainFront.append(labelTitle);
+    labelTitle.append(title1);
+    // блок back
+    mainFront.append(mainBack);
+    mainBack.append(properties);
+    // блок страна
+    properties.append(propCountry);
+    propCountry.append(labelCountry);
+    propCountry.append(country1);
+    // блок жанр
+    properties.append(propGenre);
+    propGenre.append(labelGenre);
+    propGenre.append(genre1);
+    // блок режиссер
+    properties.append(propDirector);
+    propDirector.append(labelDirector);
+    propDirector.append(director1);
+    // блок сценарий
+    properties.append(propScenario);
+    propScenario.append(labelScenario);
+    propScenario.append(scenario1);
+    // блок продюсер
+    properties.append(propProducer);
+    propProducer.append(labelProducer);
+    propProducer.append(producer1);
+    // блок оператор
+    properties.append(propOperator);
+    propOperator.append(labelOperator);
+    propOperator.append(operator1);
+    // блок композитор
+    properties.append(propComposer);
+    propComposer.append(labelComposer);
+    propComposer.append(composer1);
+    // блок бюджет
+    properties.append(propBudget);
+    propBudget.append(labelBudget);
+    propBudget.append(budget1);
+    // блок сборы
+    properties.append(propMoney);
+    propMoney.append(labelMoney);
+    propMoney.append(money1);
+    // блок рейтинг
+    properties.append(propRating);
+    propRating.append(labelRating);
+    propRating.append(rating1);
+    // блок длительность
+    properties.append(propLength);
+    propLength.append(labelLength);
+    propLength.append(length1);
+    // блок дата
+    properties.append(propDate);
+    propDate.append(labelDate);
+    propDate.append(date1);       
+}
+
+
